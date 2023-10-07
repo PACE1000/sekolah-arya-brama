368dfa7a40339dbdfae0bec364ef81d6237765ed
Arya Bramaputra <aryabrama901@gmail.com>
2023-10-07 16:05:43 +0700

    initial commit masukkan pin

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Frame 58 ver 2"
+include(":app")
+ 
